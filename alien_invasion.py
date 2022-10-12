f576618dad47a787edde0b85c5532ddf584cdd2b
neilgudwala <neil.gudwala@gmail.com>
2022-10-12 23:06:47 +0530

    Installed pygame
    module error pygame.base not found fml!

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,8 +2,8 @@ import sys
 import pygame
 from settings import Settings
 class AlienInvasion:
-     """Overall class to manage game assets and behavior."""  
-     def __init__(self):
+    """Overall class to manage game assets and behavior."""  
+    def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings=Settings()
